00da6475d1be26add9fcfcb83eb34b77bb795919
Tyler J Hague <tjhague@gmail.com>
2018-09-26 20:32:28 -0400

    Kin 7-15 ready for testing

--- emc/kin_clean.cpp
+++ emc/kin_clean.cpp
@@ -46,6 +46,38 @@ void kin_clean(int kin){
      * First combine them into one histogram with a weighted average
      * Then, move that to a new histogram and omit the first and last bin
      */
+    
+    //Combine iterations
+    TFile *f = new TFile(Form("fullKin/kin%d_1st.root",kin));
+    TH1D *i1 = (TH1D*) gDirectory->Get("emc");
+    i1->SetDirectory(0);
+    delete f;
+
+    *f = new TFile(Form("fullKin/kin%d_2nd.root",kin));
+    TH1D *i2 = (TH1D*) gDirectory->Get("emc");
+    i2->SetDirectory(0);
+    delete f;
+
+    if(i1->GetNbinsX() != i2->GetNbinsX()){
+      cout << "Plots to be combined do not have the same number of bins." << endl;
+      cout << "Make sure they are plotted the same way before using this." << endl;
+      return;
+    }
+
+    TH1D *emc = new TH1D("combined","He-3 EMC Ratio",i1->GetNbinsX(),0,1);
+
+    for(int i = 1; i <= i1->GetNbinsX(); i++){
+      if(i1->GetBinContent(i)!=0 || i2->GetBinContent(i)!=0){
+        double c1 = i1->GetBinContent(i);
+        double c2 = i2->GetBinContent(i);
+        double e1 = i1->GetBinError(i);
+        double e2 = i2->GetBinError(i);
+        emc->SetBinContent(i,(c1/(e1*e1))+(c2/(e2*e2))*((e1*e1)+(e2*e2)));
+        emc->SetBinError(i,Sqrt((e1*e1)+(e2*e2)));
+      }
+    }
+
+    //Remove first and last bin
     bool first = true;
     TH1D *cleaned = new TH1D("emc","He-3 EMC Ratio",emc->GetNbinsX(),0,1);
     for(int i = 1; i <= emc->GetNbinsX(); i++){
