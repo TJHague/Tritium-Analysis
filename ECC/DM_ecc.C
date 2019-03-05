856ca2d3ceb98f64a46111fee40a564d248a0c9e
Tyler Hague <TJHague@gmail.com>
2019-03-05 14:35:04 -0500

    Endcap Correction code
    root files contain contamination corrections - kin16 needs to be redone
    txt files contain info on endcap fitting - info is incomplete at the moment
    ecc.C does the calculations
    gaus_peak.C is for testing fits
    do_ecc.sh automates all targets/calculations

---
+++ ECC/DM_ecc.C
@@ -0,0 +1,106 @@
+#include "../headers/TRI_Main.h"
+#include "../headers/TRI_Tools.h"
+#include "../headers/rootalias.h"
+#include "../headers/cuts.h"
+
+void DM_ecc(int kin, TString target){
+  //Load Runs
+  TString targdat = "../runlists/"; targdat += target; targdat += "_kin"; targdat += kin; targdat += ".dat";
+  TString EMdat   = "../runlists/"; EMdat  += "DM_kin";                   EMdat   += kin;  EMdat  += ".dat";
+
+  TString targlist = gGet_InputFile_Var(targdat,2);
+  TString EMlist  = gGet_InputFile_Var(EMdat ,2);
+
+  vector<Int_t> targvec = gGet_RunNoChain(targlist);
+  vector<Int_t> EMvec  = gGet_RunNoChain(EMlist);
+
+  TChain *targ = new TChain("T");
+  TChain *em = new TChain("T");
+
+  for(int i=0; i<targvec.size(); i++){
+    TChain *tmp = LoadRun(targvec[i]);
+    targ->Add(tmp);
+    delete tmp;
+  }
+  for(int i=0; i<EMvec.size(); i++){
+    TChain *tmp = LoadRun(EMvec[i]);
+    em->Add(tmp);
+    delete tmp;
+  }
+
+  //First construct the cut
+
+  TCut cut = ACC(kin==16?1:0) + PID(kin==16?1:0) + W2(kin==16?1:0);
+  TCut emcut = ACC(kin==16?1:0) + PID(kin==16?1:0) + emW2(kin==16?1:0);
+
+  //Get Normalization value
+  TF1 *upfit = new TF1("upfit","gaus",-.18,-.08);
+  TF1 *downfit = new TF1("downfit","gaus",.08,.18);
+  TF1 *emupfit = new TF1("emupfit","gaus",-.18,-.08);
+  TF1 *emdownfit = new TF1("emdownfit","gaus",.08,.18);
+
+  TCanvas *c1 = new TCanvas();
+  c1->cd(0);
+  TH1D *z = new TH1D(Form("z_kin%d",kin),Form("Target Z for Kinematic %d",kin),180,-.18,.18);
+  targ->Draw((kin==16) ? Form("rpr.z>>z_kin%d",kin) : Form("rpl.z>>z_kin%d",kin) ,cut);
+
+  z->Fit("upfit","L","SAME",-.13,-.11);
+  z->Fit("downfit","L","SAME",.12,.14);
+
+  TCanvas *c2 = new TCanvas();
+  c2->cd(0);
+  TH1D *emz = new TH1D(Form("emz_kin%d",kin),Form("Empty Z for Kinematic %d",kin),180,-.18,.18);
+  em->Draw((kin==16) ? Form("rpr.z>>emz_kin%d",kin) : Form("rpl.z>>emz_kin%d",kin) ,emcut);
+
+  emz->Fit("emupfit","L","SAME",-.13,-.11);
+  emz->Fit("emdownfit","L","SAME",.12,.14);
+
+  //Can get standard dev with GetParameter(2) and center with GetParameter(1)
+  double up_center = upfit->GetParameter(1);
+  double up_stdev = upfit->GetParameter(2);
+  double down_center = downfit->GetParameter(1);
+  double down_stdev = downfit->GetParameter(2);
+  double targ_up_count = targ->GetEntries(cut + TCut(Form((kin==16) ? "rpr.z>%f-%f" : "rpl.z>%f-%f" ,up_center,up_stdev)) + TCut(Form((kin==16) ? "rpr.z<%f+%f" : "rpl.z<%f+%f" ,up_center,up_stdev)));
+  double em_up_count = em->GetEntries(emcut + TCut(Form((kin==16) ? "rpr.z>%f-%f" : "rpl.z>%f-%f" ,up_center,up_stdev)) + TCut(Form((kin==16) ? "rpr.z<%f+%f" : "rpl.z<%f+%f" ,up_center,up_stdev)));
+  double targ_down_count = targ->GetEntries(cut + TCut(Form((kin==16) ? "rpr.z>%f-%f" : "rpl.z>%f-%f" ,down_center,down_stdev)) + TCut(Form((kin==16) ? "rpr.z<%f+%f" : "rpl.z<%f+%f" ,down_center,down_stdev)));
+  double em_down_count = em->GetEntries(emcut + TCut(Form((kin==16) ? "rpr.z>%f-%f" : "rpl.z>%f-%f" ,down_center,down_stdev)) + TCut(Form((kin==16) ? "rpr.z<%f+%f" : "rpl.z<%f+%f" ,down_center,down_stdev)));
+
+  //Get the center of the upstream and downstream cuts
+  //This will define where the contimanation comes from
+  int it;
+  if(kin<=5){
+    it=kin;
+  }else if(kin==16){
+    it=12;
+  }else{
+    it=((kin-5)/2)+5;
+  }
+  double center = (up_cut[it] + down_cut[it])/2.;
+
+  //Plot EM/Targ for z region of the kinematic
+  TString x = (kin==16) ? "EKRxe.x_bj" : "EKLxe.x_bj";
+  TString emx = (kin==16) ? "EKRx.x_bj" : "EKLx.x_bj";
+
+  TH1D *ecc_plot = new TH1D(Form("ecc_kin%d",kin),Form("Kinematic %d Endcap Contamination",kin),33,0,0.99);
+  TH1D *em_down  = new TH1D("em_down","Temporary EM Plot",33,0,0.99);
+  TH1D *targ_plot = new TH1D("targtmp","Temporary Target Plot",33,0,0.99);
+
+  TCanvas *c3 = new TCanvas();
+  c3->cd(0);
+  em->Draw(Form("%s>>ecc_kin%d",emx.Data(),kin),emcut + TCut(Form((kin==16) ? "rpr.z>%f" : "rpl.z>%f" ,up_cut[it])) + TCut(Form((kin==16) ? "rpr.z<%f" : "rpl.z<%f" ,center)));
+  TCanvas *c4 = new TCanvas();
+  c4->cd(0);
+  em->Draw(Form("%s>>em_down",emx.Data()),emcut + TCut(Form((kin==16) ? "rpr.z>%f" : "rpl.z>%f" ,center)) + TCut(Form((kin==16) ? "rpr.z<%f" : "rpl.z<%f" ,down_cut[it])));
+  TCanvas *c5 = new TCanvas();
+  c5->cd(0);
+  targ->Draw(x+">>targtmp",cut + EC(0));
+
+  ecc_plot->Scale(targ_up_count/em_up_count);
+  em_down->Scale(targ_down_count/em_down_count);
+  ecc_plot->Add(em_down);
+  ecc_plot->Divide(targ_plot);
+
+  TFile *f = new TFile("DM_"+target+".root","UPDATE");
+  ecc_plot->Write();
+  f->Close();
+}
---
+++ ECC/gaus_peak.C
@@ -0,0 +1,35 @@
+#include "../headers/TRI_Main.h"
+#include "../headers/TRI_Tools.h"
+#include "../headers/rootalias.h"
+#include "../headers/cuts.h"
+
+void gaus_peak(int kin){
+  //Load Runs
+  TString EMdat = "./runlists/"; EMdat += "DM_kin"; EMdat += kin; EMdat += ".dat";
+
+  TString EMlist = gGet_InputFile_Var(EMdat ,2);
+
+  vector<Int_t> EMvec = gGet_RunNoChain(EMlist);
+
+  TChain *em = new TChain("T");
+
+  for(int i=0; i<EMvec.size(); i++){
+    TChain *tmp = LoadRun(EMvec[i]);
+    em->Add(tmp);
+    delete tmp;
+  }
+
+  //Plot
+  //Construct the cut
+  TCut cut = ACC(kin==16?1:0) + PID(kin==16?1:0) + W2(kin==16?1:0);
+  cut += (kin==16) ? "abs(rpr.z)<.18" : "abs(rpl.z)<.18";
+
+  TH1D *z = new TH1D(Form("z_kin%d",kin),Form("Target Z for Kinematic %d",kin),180,-.18,.18);
+  em->Draw((kin==16) ? Form("rpr.z>>z_kin%d",kin) : Form("rpl.z>>z_kin%d",kin) ,cut);
+
+  TF1 *upfit = new TF1("upfit","gaus",-.18,-.08);
+  TF1 *downfit = new TF1("downfit","gaus",.08,.18);
+
+  z->Fit("upfit","L","SAME",-.18,-.08);
+  z->Fit("downfit","L","SAME",.08,.18);
+}
