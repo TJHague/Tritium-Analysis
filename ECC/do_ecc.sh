652ad7f629094fa10d216bbfe4f578882466bd15
Tyler Hague <TJHague@gmail.com>
2019-06-28 14:34:24 -0400

    Missing ECC updates

--- ECC/do_ecc.sh
+++ ECC/do_ecc.sh
@@ -4,13 +4,11 @@ declare -a targs=("H1" "He3" "D2" "H3")
 
 for i in "${targs[@]}"
 do
-  for j in `seq 0 16`;
+  for j in `seq 0 15`;
   do
     if [ $j -lt 6 ] || [ $(( $j%2 )) -eq 1 ] || [ $j -eq 16 ];
     then
-      analyzer -l -q 'ecc.C('$j',"'"$i"'",1.5,1.5,"sig1_5")'
-      analyzer -l -q 'ecc.C('$j',"'"$i"'",2,2,"sig2")'
-      analyzer -l -q 'ecc.C('$j',"'"$i"'",0,2,"sigout2")'
+      analyzer -l -q 'ecc.C('$j',"'"$i"'",0,2,"out2_ACCfinal")'
     fi
   done
 done
--- ECC/do_sep.sh
+++ ECC/do_sep.sh
@@ -4,12 +4,12 @@ declare -a targs=("H1" "He3" "D2" "H3")
 
 for i in "${targs[@]}"
 do
-  for j in `seq 0 15`;
+  for j in `seq 7 16`;
   do
-    if [ $j -lt 6 ] || [ $(( $j%2 )) -eq 1 ] || [ $j -eq 16 ];
+    if [ $j -lt 6 ];
     then
       analyzer -l -q 'ecc.C('$j',"'"$i"'",0,2,"out2_sep")'
-    elif [ $j -gt 6 && $(( $j%2 )) -eq 1 && $j -ne 15 ] || [ $j -eq 16 ] 
+    elif [ $j -gt 6 ] && [ $(( $j%2 )) -eq 1 ] && [ $j -ne 15 ] || [ $j -eq 16 ] 
     then
       analyzer -l -q 'ecc_separate.C('$j',"'"$i"'",0,2,"out2_sep",1)'
       analyzer -l -q 'ecc_separate.C('$j',"'"$i"'",0,2,"out2_sep",2)'
